868f21d62a91b64959a3df4c43972864d7c131c4
1303LIZ <mwaranganga@gmail.com>
2025-07-18 20:10:15 +0300

    added tips logicality

--- recycle_tips/templatetags/recycle_tips_extras.py
+++ recycle_tips/templatetags/recycle_tips_extras.py
@@ -6,3 +6,14 @@ register = template.Library()
 def get_item(dictionary, key):
     """Template filter to get an item from a dictionary using a key"""
     return dictionary.get(key, 0)
+
+@register.filter
+def sum_values(value):
+    """Template filter to sum the values of a dictionary or iterable"""
+    if hasattr(value, 'values'):
+        # If it's a dictionary, sum its values
+        return sum(value.values())
+    elif hasattr(value, '__iter__'):
+        # If it's an iterable, sum its items
+        return sum(value)
+    return 0
